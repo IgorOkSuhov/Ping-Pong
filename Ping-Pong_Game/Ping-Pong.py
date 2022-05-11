ed8826f1476c6e7c6a18b0d41d55e36de7de49a4
IgorOk <igoroksuhov060195@gmail.com>
2022-05-11 16:05:52 +0200

    create window pong game

---
+++ Ping-Pong_Game/Ping-Pong.py
@@ -0,0 +1,9 @@
+import turtle
+# Creating a playing field
+
+window = turtle.Screen()
+window.title('The Game of year Edition: PING-PONG GAME =)')
+window.setup(width=1.0,height=1.0)
+window.bgcolor('black')
+
+
