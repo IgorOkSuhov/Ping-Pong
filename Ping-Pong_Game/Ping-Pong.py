c7c11e75f3c49b182526d28e927d43400e13064b
IgorOk <igoroksuhov060195@gmail.com>
2022-05-11 16:25:27 +0200

    create basic logic

--- Ping-Pong_Game/Ping-Pong.py
+++ Ping-Pong_Game/Ping-Pong.py
@@ -6,4 +6,12 @@ window.title('The Game of year Edition: PING-PONG GAME =)')
 window.setup(width=1.0,height=1.0)
 window.bgcolor('black')
 
+# Denote the boundary of the field
 
+border = turtle.Turtle()
+border.color('green')
+border.goto(-500,300)
+border.goto(500,300)
+border.goto(500,-300)
+border.goto(-500,-300)
+window.mainloop()
