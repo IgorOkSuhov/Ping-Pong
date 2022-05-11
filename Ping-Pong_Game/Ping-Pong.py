aba14a6899708a364b8a3915cd6fa6de0d6babb4
IgorOk <igoroksuhov060195@gmail.com>
2022-05-11 16:54:07 +0200

    create basic logic vol2

--- Ping-Pong_Game/Ping-Pong.py
+++ Ping-Pong_Game/Ping-Pong.py
@@ -9,9 +9,29 @@ window.bgcolor('black')
 # Denote the boundary of the field
 
 border = turtle.Turtle()
+border.speed(0)
 border.color('green')
+border.begin_fill()
 border.goto(-500,300)
 border.goto(500,300)
 border.goto(500,-300)
 border.goto(-500,-300)
+border.goto(-500,300)
+border.end_fill()
+
+# Create middle
+
+border.goto(0,300)
+border.color('white')
+border.setheading(270)
+for i in range(25):
+    if i % 2 == 0:
+        border.forward(24)
+    else:
+        border.up()
+        border.forward(24)
+        border.down()
+border.hideturtle()
+
+
 window.mainloop()
